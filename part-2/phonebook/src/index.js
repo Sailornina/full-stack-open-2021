c207925d67aaca959defbcb74f5736c5b2206826
Sailornina <sylcardozo.sc@gmail.com>
2021-11-15 23:12:04 +0100

    the phonebook step 6

--- part-2/phonebook/src/index.js
+++ part-2/phonebook/src/index.js
@@ -3,10 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const notes = [
+  {
+    "persons":[
+      { 
+        "name": "Arto Hellas", 
+        "number": "040-123456",
+        "id": 1
+      },
+      { 
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523",
+        "id": 2
+      },
+      { 
+        "name": "Dan Abramov", 
+        "number": "12-43-234345",
+        "id": 3
+      },
+      { 
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122",
+        "id": 4
+      }
+    ]
+  }
+]
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App notes={notes} />, rootElement);
 
