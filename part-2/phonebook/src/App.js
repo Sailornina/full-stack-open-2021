c4d760deef305eb5667efff376acb91a0c7a2922
Sailornina <sylcardozo.sc@gmail.com>
2022-05-03 21:44:09 +0200

    step 6 in progress

--- part-2/phonebook/src/App.js
+++ part-2/phonebook/src/App.js
@@ -1,21 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PersonForm from "./components/PersonForm";
 import PersonList from "./components/PersonList";
 import Filter from './components/Filter';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  
-  const [newFilter, setNewFilter] = useState('');
+  const [ persons, setPersons ] = useState([]);
+  const [ newFilter, setNewFilter ] = useState('');
   
   const filteredNames = persons.filter(person =>
     person.name.toUpperCase().includes(newFilter.toLowerCase())
-    );
+  );
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+    }, [])
+
+  console.log('render', persons.length, 'persons')
 
   return (
     <div>
@@ -25,7 +32,7 @@ const App = () => {
       <PersonForm persons={persons} setPersons={setPersons}/>
       <PersonList persons={filteredNames} />
     </div>
-  )
+  );
 };
 
 export default App;
