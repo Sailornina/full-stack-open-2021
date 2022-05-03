7dd7dd4b976e897eaa35da085a17f833efdf2ff5
Sailornina <sylcardozo.sc@gmail.com>
2022-05-03 17:15:47 +0200

    axios part is done

--- part-2/phonebook/src/index.js
+++ part-2/phonebook/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import axios from 'axios';
+import App from './App';
 
-const promise = axios.get('http://localhost:3001/notes')
-console.log(promise)
+axios
+  .get('http://localhost:3001/notes')
+  .then(response => {
+    const notes = response.data 
+    ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
+})
 
 const promise2 = axios.get('http://localhost:3001/foobar')
 console.log(promise2)
