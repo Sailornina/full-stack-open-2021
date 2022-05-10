db5f0975ff83f950849a78872c2d7aafcf9a2aee
Sailornina <sylcardozo.sc@gmail.com>
2022-05-10 18:21:04 +0200

    content in progress

--- part-2/countries/src/components/Content.js
+++ part-2/countries/src/components/Content.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 const Content = ({ countries, setCountries }) => {
+    if(countries.length > 10) {
     return ( 
         <p>Too many matches, specify another filter</p>
-    );
+    )};
     
 };
 
