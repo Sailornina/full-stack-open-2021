e2a1b61d059e2019004380a7953547e7a351e5d0
Sailornina <sylcardozo.sc@gmail.com>
2022-05-10 17:45:17 +0200

    countries 2.12 step 1 in progress

--- part-2/countries/src/App.js
+++ part-2/countries/src/App.js
@@ -1,15 +1,42 @@
 import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import Filter from './components/Filter';
+import Content from './components/Content'
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [allCountries, setAllCountries] = useState([])
+  const [newFilter, setNewFilter] = useState('')
 
 
+ useEffect(() => {
+  axios
+    .get('https://restcountries.com/v3.1/all')
+    .then((response) => {
+      const { data } = response
+      setAllCountries(data)
+    })
+ }, [])
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+ const handleFilterChange = (event) => {
+   console.log("handleFilterChange called")
+   const filter = event.target.value
+   setNewFilter(event.target.value)
+   if (filter.length <= 1) {
+     return;
+   }   
+  
+  const regex = new RegExp(newFilter, 'i' );
+  const filteredCountries = allCountries.filter(country => country.name.common.match(regex))
+  setCountries(filteredCountries)
+ }
+
+
+ return (
+    <div className="countries">
+    <Filter value={newFilter} onChange={handleFilterChange} />
+    <Content countries={countries} setCountries={setCountries}/>
+    {countries.map((country) => <p>{country.name.common}</p>)}
     </div>
   );
 }
---
+++ part-2/countries/src/components/Content.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Content = ({ countries, setCountries }) => {
+    return ( 
+        <p>Too many matches, specify another filter</p>
+    );
+    
+};
+
+export default Content;
