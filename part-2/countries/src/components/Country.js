ba0ac5ce3fcba3b58b56229113200f3162a3ef68
Sailornina <sylcardozo.sc@gmail.com>
2022-05-14 15:53:25 +0200

    2.14- Countries step3 is done.

--- part-2/countries/src/components/Country.js
+++ part-2/countries/src/components/Country.js
@@ -3,11 +3,13 @@ import axios from 'axios';
 
 const api_key = process.env.REACT_APP_API_KEY
 
+
 const Country = ({ country }) => {
     const [weather, setWeather] = useState({})
 
     useEffect(() => {
-        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Stockholm&appid=${api_key}`)
+        const capital = country.capital
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${api_key}`)
             .then((response) => {
                 const { data } = response
                 const [weather] = data.weather
